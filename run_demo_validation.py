3ebff5737b753a8245fabc1cb68c91144e5b470f
Ankita7977 <ankitaprajapati959457@gmail.com>
2026-03-26 10:02:59 +0530

    Add files via upload

---
+++ run_demo_validation.py
@@ -0,0 +1,21 @@
+from src.validation.signal_validator import validate_signal
+
+signals = [
+    {
+        "signal_id": 1,
+        "dataset_id": 1,
+        "timestamp": "2025-03-25 10:30:00",
+        "latitude": 28.6,
+        "longitude": 77.2,
+        "feature_type": "movement",
+        "value": 45
+    },
+    {
+        "signal_id": 2,
+        "timestamp": "2025-03-25 10:30:00"
+    }
+]
+
+for s in signals:
+    result = validate_signal(s)
+    print(result)
